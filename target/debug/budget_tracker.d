f6e12ee1babc67957f1184b626a33ede19c7b484
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-09-03 03:40:52 +0300

    Rust Capstone

---
+++ target/debug/budget_tracker.d
@@ -0,0 +1,1 @@
+/home/tony/Desktop/moringa-ai-capstone-project/rust_beginner_project/target/debug/budget_tracker: /home/tony/Desktop/moringa-ai-capstone-project/rust_beginner_project/src/main.rs
---
+++ target/debug/deps/budget_tracker-44ef828921e391d7.d
@@ -0,0 +1,5 @@
+/home/tony/Desktop/moringa-ai-capstone-project/rust_beginner_project/target/debug/deps/budget_tracker-44ef828921e391d7.d: src/main.rs
+
+/home/tony/Desktop/moringa-ai-capstone-project/rust_beginner_project/target/debug/deps/budget_tracker-44ef828921e391d7: src/main.rs
+
+src/main.rs:
